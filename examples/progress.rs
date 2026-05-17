c03e469b660ab800325bd927c0dd8b8fcb31efdc
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-05-17 11:37:23 +0800

    feat: Example progress bar

--- examples/progress.rs
+++ examples/progress.rs
@@ -3,20 +3,27 @@ use std::io::Result;
 use tokio::time::{Duration, sleep};
 
 const DURATION: Duration = Duration::from_millis(50);
+const PROGRESS_BAR_CHAR: char = '█';
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let mut progress = Progress::default();
-    progress.start();
-
-    for _ in 0..=100 {
-        work().await;
-        progress.increment(1).flush()?;
-    }
+    test_progress().await?;
 
     Ok(())
 }
 
-async fn work() {
-    sleep(DURATION).await;
+async fn test_progress() -> Result<()> {
+    let mut progress = Progress::default();
+    progress.start();
+
+    for _ in 0..=100 {
+        sleep(DURATION).await;
+        progress.increment(1).flush()?;
+        let width = progress.get_progress() as usize * 50 / 100;
+        let bar = PROGRESS_BAR_CHAR.to_string().repeat(width);
+        eprint!("[{:50}] {}%\r", bar, progress.get_progress());
+    }
+    eprintln!();
+
+    Ok(())
 }
